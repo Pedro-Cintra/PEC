7d8e851462c78b29209698520b95d4aa7ef62869
Pedro Henrique de Oliveira Cintra <pcintra@furb.br>
2023-08-12 00:36:09 -0300

    Ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>
-      Seja bem vindo ao PEC - Plataforma de Estudos Compartilhados
-    </div>
+    <div className=" flex justify-center mt-6">
+      <h1 className="border-4 p-4 bg-teal-300 font-sans">
+        Seja bem vindo ao PEC - Plataforma de Estudos Compartilhados
+      </h1>   
+         
+    </div>    
   )
 }
